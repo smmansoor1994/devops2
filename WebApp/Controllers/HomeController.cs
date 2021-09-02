e1954a66376bb6d38cf04be0038123de60171859
smmansoor1994 <59280383+smmansoor1994@users.noreply.github.com>
2021-09-02 19:37:16 +0530

    2

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration configuration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration _configuration)
+        {
+            _logger = logger;
+            configuration = _configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["ApiKey"] = configuration["ApiKey"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
